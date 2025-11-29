305e0fa1e6a2b2852bc7d751e6add6f26d6a0f44
Morulo90 <rubenhuetor@gmail.com>
2025-11-29 14:17:06 +0100

    Despliegue exitoso en Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    // Aquí definimos la red local para pruebas rápidas
+    hardhat: {},
+    // Aquí definimos la red Sepolia (Internet real de pruebas)
+    sepolia: {
+      url: "https://sepolia.drpc.org",
+      accounts: ["62d051d2a11f65c6d7f4c159a13d542ebedb0ee11862addf7b02b6224c10dd0f"]
+    }
+  }
 };
 
 export default config;
