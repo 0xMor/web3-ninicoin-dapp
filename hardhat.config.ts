483386ebdfa2c8ee3dff21ab5812714f54d18a11
Morulo90 <rubenhuetor@gmail.com>
2025-12-04 14:18:21 +0100

    feat: Integrated Chainlink Oracle for ETH/USD price

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: "https://sepolia.drpc.org",
       // ¡AQUÍ ES DONDE BORRAS TU CLAVE REAL!
-      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
+      accounts: ["TU_CLAVE_PRIVADA_AQUI_NO_SUBIR0dd0f"]
     }
   },
   etherscan: {
